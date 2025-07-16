8f3f4921213806bf1fa46286925af5b80af61863
Adam_Gadaczek <adam.gadaczek@wp.pl>
2025-07-16 10:17:31 +0200

    Use .capitalize() for shop and product names

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 shopping_dict = {
-    "Piekarnia": ["Chleb", "Pączek", "Bułki"],
-    "Warzywniak": ["Marchew", "Pomidor", "Ogórek"]
+    "piekarnia": ["chleb", "pączek", "bułki"],
+    "warzywniak": ["marchew", "pomidor", "ogórek"]
 }
 
 for shop in shopping_dict:
-    products = shopping_dict[shop]
-    print(f"Idę do {shop} i kupuję tam {products}.")
+    products = [item.capitalize() for item in shopping_dict[shop]]
+    print(f"Idę do {shop.capitalize()} i kupuję tam: {', '.join(products)}.")
