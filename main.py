e1118ca06a2e743342d72a5fe2dfa36f4ccdbc96
Adam_Gadaczek <adam.gadaczek@wp.pl>
2025-07-16 10:07:11 +0200

    Add for loop to display shopping list

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ shopping_dict = {
     "Piekarnia": ["Chleb", "Pączek", "Bułki"],
     "Warzywniak": ["Marchew", "Pomidor", "Ogórek"]
 }
+
+for shop in shopping_dict:
+    products = shopping_dict[shop]
+    print(f"Idę do {shop} i kupuję tam {products}.")
